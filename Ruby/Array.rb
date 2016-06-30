34ca42e7663aaf2c2ca12866c3db2af737d05c2b
richard6428 <richar6428@hotmail.com>
2016-06-29 22:15:57 -0500

    Moificacion array

---
+++ Ruby/Array.rb
@@ -0,0 +1,70 @@
+=begin Metodo de Sustitucion
+
+a = "richard brandon rios jaramillo"
+puts a.gsub("r", "R")
+
+=end
+
+=begin Contar todos los caracteres del string
+
+a = "richard brandon rios jaramillo".length
+puts a
+
+=end
+
+=begin Muestra el string alreves
+  
+a = "richard brandon rios jaramillo".reverse
+puts a
+  
+=end
+
+=begin Cambiar a Mayuscula
+
+a = "richard brandon rios jaramillo".upcase
+puts a
+
+=end
+
+=begin Cambiar a Minuscula
+
+a = "richard brandon rios jaramillo".downcase
+puts a
+
+=end
+
+=begin La primera letra en Mayuscula
+
+a = "richard brandon rios jaramillo".capitalize
+puts a
+
+=end
+
+
+=begin
+
+objects = [1, 2, 3, 4, 5, 6]
+objects.each do |num|
+	puts num
+end
+
+objects.each  { |num| puts num }
+
+a = [ 5, 3, 2, 4, 7, 1, 8 ]
+a.delete (3)
+a.drop(3)
+puts a
+=end
+
+=begin
+n = [4, 5, 8, 3, 9]
+n.each {|x| puts x if x%3==0 }
+=end
+
+=begin 
+
+a = [4, 5, 8, 3, 9]
+b= [ 5, 3, 2, 4, 7]
+
+a.map{|c|c + b.each}
+=end
