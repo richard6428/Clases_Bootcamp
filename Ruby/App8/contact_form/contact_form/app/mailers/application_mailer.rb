bac3a2dd191c59b965683921d6cae08e88a2b898
richard6428 <richar6428@hotmail.com>
2016-08-04 10:30:33 -0500

    nuevas apps

---
+++ Ruby/App8/contact_form/contact_form/app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "railsgirlsbarranquilla@gmail.com"
+  layout 'mailer'
+end
---
+++ Ruby/App9/devise_example/app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+
+	 before_action :authenticate_user!, only: [:index]
+  def index
+  end
+
+  def indx
+  end
+end
